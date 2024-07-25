d2866a64178e08ae1581286b4da6e34bf49a7f5f
Michał <mwarioxin@gmail.com>
2024-07-25 22:08:33 +0200

    Added converting Room model to dict along with two tests

--- tests/domain/test_room.py
+++ tests/domain/test_room.py
@@ -35,3 +35,32 @@ def test_room_model_from_dict():
     assert init_dict["price"] == 100
     assert init_dict["longitude"] == -0.09998975
     assert init_dict["latitude"] == 51.75436293
+
+
+def test_room_model_to_dict():
+    code = uuid.uuid4()
+    init_dict = {
+        "code": code,
+        "size": 10,
+        "price": 100,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293,
+    }
+    room = Room.from_dict(init_dict)
+
+    assert room.to_dict() == init_dict
+
+
+def test_room_model_comparison():
+    code = uuid.uuid4()
+    init_dict = {
+        "code": code,
+        "size": 10,
+        "price": 100,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293,
+    }
+    room1 = Room.from_dict(init_dict)
+    room2 = Room.from_dict(init_dict)
+
+    assert room1 == room2
